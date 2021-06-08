12e2043fe70a8e7c99c472da35cf867a864b8ae0
Kateryna Hshanovych <a.cherewaty@gmail.com>
2021-06-08 22:55:35 +0300

    create navbar and slider

--- index.php
+++ index.php
@@ -6,8 +6,173 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- CSS only -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navigation">
+            <div class="container-xxl">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item navigation__item">
+                            <a class="nav-link navigation__link" href="#">Info</a>
+                        </li>
+                        <li class="nav-item navigation__item">
+                            <a class="nav-link navigation__link" href="#">Team</a>
+                        </li>
+                        <li class="nav-item navigation__item">
+                            <a class="nav-link disabled navigation__link" href="#" tabindex="-1" aria-disabled="true">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+        <div class="container-xxl animals">
+            <div class="row align-items-center">
+                <div class="col-7">
+                    <h1 class="animals__title">Welcome to Animal home</h1>
+                </div>
+                <div class="col-5">
+                    <img class="w-100" src="./img/dog.jpeg" alt="dog" title="dog">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+    <div class="container-xxl">
+        <div class="row align-items-center">
+            <div class="col-7">
+                <h2>Our animal</h2>
+            </div>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_two.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Doggy</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Doggy</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="./img/dog_one.jpeg" class="card-img-top" alt="dog">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Doggy</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
 <body>
-<h1>777</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
